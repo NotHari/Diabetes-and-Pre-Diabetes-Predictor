c232ac4e31f0174c8165dbdb25f1a08b27dc630d
Kaushik SP <77889386+kaushik-3009@users.noreply.github.com>
2022-04-07 13:26:51 +0530

    testing

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ import json
 model = pickle.load(open("models/model.pkl", "rb"))
 
 # Generate some data for validation
-X_test, y = make_regression(100,n_features = 8)
+X_test, y = make_regression(100, n_features=8)
 
 # Test on the model
 y_hat = model.predict(X_test)
+# testing svn
