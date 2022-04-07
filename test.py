402f23e5d008654ea06e12573fe827994480a4ce
Harikrishnan Nair <80103986+NotHari@users.noreply.github.com>
2022-04-07 11:48:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pickle
 from sklearn.datasets import make_regression
 import json
 
-model = pickle.load(open("models/model_pkl.pkl", "rb"))
+model = pickle.load(open("models/model.pkl", "rb"))
 
 # Generate some data for validation
 X_test, y = make_regression(100,n_features = 8)
